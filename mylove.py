a855081a0921fe95f9639740cd847fecda77269e
carmelynnime <143869826+carmelynnime@users.noreply.github.com>
2024-10-28 14:19:08 +0800

    file name update mylove.py

--- mylove.py
+++ mylove.py
@@ -69,9 +69,9 @@ if 'start' in st.session_state:
         
         memories = {
             "🌹 First Glance": ("photos/IMG_9399.JPG", "Seeing this photo of us still makes me blush and smile🥰. Who would have thought that the guy I was only seeing on MyDays would grab my waist during our first meet? It's so surreal, you knaaauuur? HAHAHA. This was the day I first heard your voice, and I was honestly shocked by how it sounded. It was unusual but cute😅. I didn’t expect you to come and spare your precious time just to see me perform at this event. It meant a lot to me, especially since we had only known each other for days. Also, thank you for buying me Sting! HAHAHA, so funny that I got you to do errands on day one. Thank you again for coming Dreeeeee, I appreciate it sauuurr much!"),
-            "💫 Magical Skinship": ("photos/IMG_7459.JPG", "The feeling might not be new to you, but it was for me. I don’t easily let other men hold my hand, so I was a bit nervous when you started playing with my hands. I hadn’t held hands with anyone for quite a while and thank you for taking a picture it’s one of my favorites! This was the moment when I started feeling comfortable talking to you. I stopped lying and decided to tell you the truth. Maybe I gave in and decided to trust you. It was hard for me to share, especially knowing you liked me (or ambot AAHAHAH assuming lang AHHAHAH). I didn’t want you to pity me or treat me differently because of my situation. I can stand on my own, and I don’t want to depend on anyone. Despite all the negative thoughts running through my mind, you listened. You sincerely listened and respected me, and you even reminded me that if I wasn’t comfortable sharing, I didn’t have to. You let me take my time. Thank you for making me feel safe enough to trust you. I had fun talking about my life with you, and again, thank you for listening."),
-            "✨ Growing Closer": ("photos/IMG_0058.JPeG", "This was the day I purposely pushed you away because I was scared of all the possibilities that could happen if we continued communicating. I was afraid of losing you. I didn’t want to get attached. I was terrified that I’d be building my man for another woman. This was also the time when you first kissed me on the cheek and kept playing with my shoulders while I giggled 😬. Before we went to Il Corso, I remember being so shy during dinner, but my shyness disappeared because you made me so comfortable. Time flew by so fast, and we eventually went to our third unplanned stop, Bay Coffee. I appreciate every time you take the initiative, like ordering food for us, even though you mistakenly ordered the donut😂. But it was still yummy, so it’s fine, cutie boy! I still got shy when you looked at me at this point… hays, my crush HHAHAHA. I love this pic of ours, bc you really go along with my craziness."),
-            "💞 Falling in Love": ("photos/IMG_0100.JPG", "We had dinner at Sugbo Mercado before taking this pic. Thanks for the treat I was so full! Anyways, I can’t explain how we look so cute together 😩 AW AHAHHAHA. I still felt shy and awkward during physical contact poses, but thank you for making me comfortable again! This day I felt a bit nervous. Your pace in getting to know each other was too fast for me. It was too fast because I wasn’t used to it. The plan was supposedly just cuddles. I wasn’t supposed to give you our first kiss at QL, GRAAAAAAAAAAAAAAAAAH, but I just couldn’t resist your cuteness when you said, “Pleaseeee.” You kept trying to kiss me, imagine how cute you wereeee!! Until I got carried away and gave in!! We shared our first sweet kiss and I just want you to know you are so gentle and passionate when you kiss. I love the way how you do it."),
+            "💫 Magical Skinship": ("photos/IMG_7459.jpg", "The feeling might not be new to you, but it was for me. I don’t easily let other men hold my hand, so I was a bit nervous when you started playing with my hands. I hadn’t held hands with anyone for quite a while and thank you for taking a picture it’s one of my favorites! This was the moment when I started feeling comfortable talking to you. I stopped lying and decided to tell you the truth. Maybe I gave in and decided to trust you. It was hard for me to share, especially knowing you liked me (or ambot AAHAHAH assuming lang AHHAHAH). I didn’t want you to pity me or treat me differently because of my situation. I can stand on my own, and I don’t want to depend on anyone. Despite all the negative thoughts running through my mind, you listened. You sincerely listened and respected me, and you even reminded me that if I wasn’t comfortable sharing, I didn’t have to. You let me take my time. Thank you for making me feel safe enough to trust you. I had fun talking about my life with you, and again, thank you for listening."),
+            "✨ Growing Closer": ("photos/IMG_0058.jpeg", "This was the day I purposely pushed you away because I was scared of all the possibilities that could happen if we continued communicating. I was afraid of losing you. I didn’t want to get attached. I was terrified that I’d be building my man for another woman. This was also the time when you first kissed me on the cheek and kept playing with my shoulders while I giggled 😬. Before we went to Il Corso, I remember being so shy during dinner, but my shyness disappeared because you made me so comfortable. Time flew by so fast, and we eventually went to our third unplanned stop, Bay Coffee. I appreciate every time you take the initiative, like ordering food for us, even though you mistakenly ordered the donut😂. But it was still yummy, so it’s fine, cutie boy! I still got shy when you looked at me at this point… hays, my crush HHAHAHA. I love this pic of ours, bc you really go along with my craziness."),
+            "💞 Falling in Love": ("photos/IMG_0100.jpg", "We had dinner at Sugbo Mercado before taking this pic. Thanks for the treat I was so full! Anyways, I can’t explain how we look so cute together 😩 AW AHAHHAHA. I still felt shy and awkward during physical contact poses, but thank you for making me comfortable again! This day I felt a bit nervous. Your pace in getting to know each other was too fast for me. It was too fast because I wasn’t used to it. The plan was supposedly just cuddles. I wasn’t supposed to give you our first kiss at QL, GRAAAAAAAAAAAAAAAAAH, but I just couldn’t resist your cuteness when you said, “Pleaseeee.” You kept trying to kiss me, imagine how cute you wereeee!! Until I got carried away and gave in!! We shared our first sweet kiss and I just want you to know you are so gentle and passionate when you kiss. I love the way how you do it."),
         }
         
         selected_memory = st.selectbox("Choose a memory", list(memories.keys()))
@@ -88,25 +88,25 @@ if 'start' in st.session_state:
 
         col1, col2 = st.columns(2)
         with col1:
-            st.image("photos/IMG_0059.JPG", caption="July 06, 2024")
+            st.image("photos/IMG_0059.jpg", caption="July 06, 2024")
         with col2:
-            st.image("photos/IMG_1170.JPG", caption="July 28, 2024")
+            st.image("photos/IMG_1170.jpg", caption="July 28, 2024")
         with col1:
-            st.image("photos/IMG_1203.JPG", caption="July 28, 2024")
+            st.image("photos/IMG_1203.jpg", caption="July 28, 2024")
         with col2:
-            st.image("photos/IMG_2106.JPG", caption="August 09, 2024")
+            st.image("photos/IMG_2106.jpg", caption="August 09, 2024")
         with col1:
-            st.image("photos/IMG_2346.JPG", caption="August 23, 2024")
+            st.image("photos/IMG_2346.jpg", caption="August 23, 2024")
         with col2:
-            st.image("photos/IMG_2466.JPG", caption="August 23, 2024")
+            st.image("photos/IMG_2466.jpg", caption="August 23, 2024")
         with col1:
-            st.image("photos/IMG_3258.JPG", caption="August 27, 2024")
+            st.image("photos/IMG_3258.jpg", caption="August 27, 2024")
         with col2:
-            st.image("photos/IMG_3431.JPG", caption="September 08, 2024")
+            st.image("photos/IMG_3431.jpg", caption="September 08, 2024")
         with col1:
-            st.image("photos/IMG_3456.JPG", caption="September 09, 2024")
+            st.image("photos/IMG_3456.jpg", caption="September 09, 2024")
         with col2:
-            st.image("photos/IMG_4416.JPG", caption="First delivered food by Andreeee! Appreciate it so much🥰!")
+            st.image("photos/IMG_4416.jpg", caption="First delivered food by Andreeee! Appreciate it so much🥰!")
     
     # Cutie Dates Page 
     def cute_date():
@@ -114,23 +114,23 @@ if 'start' in st.session_state:
         
         st.write("**To more dates together with you Dree!!!**")
 
-        header_image = Image.open("photos/IMG_0093.JPG")  # Add your header image path here
+        header_image = Image.open("photos/IMG_0093.jpg")  # Add your header image path here
         st.image(header_image, caption="First Photobooth!", use_column_width=True)
-        header_image = Image.open("photos/IMG_0936.JPG")  
+        header_image = Image.open("photos/IMG_0936.jpg")  
         st.image(header_image, caption="First Trip Together!", use_column_width=True)
-        header_image = Image.open("photos/IMG_1615.JPG")  
+        header_image = Image.open("photos/IMG_1615.jpg")  
         st.image(header_image, caption="First at Tops!", use_column_width=True)
-        header_image = Image.open("photos/IMG_2438.JPG")  
+        header_image = Image.open("photos/IMG_2438.jpg")  
         st.image(header_image, caption="First Arcade!", use_column_width=True)
-        header_image = Image.open("photos/IMG_2493.JPG")  
+        header_image = Image.open("photos/IMG_2493.jpg")  
         st.image(header_image, caption="First Movie Date!", use_column_width=True)
-        header_image = Image.open("photos/IMG_3252.JPG")  
+        header_image = Image.open("photos/IMG_3252.jpg")  
         st.image(header_image, caption="First Long Ride!", use_column_width=True)
-        header_image = Image.open("photos/IMG_2707.JPG")  
+        header_image = Image.open("photos/IMG_2707.jpg")  
         st.image(header_image, caption="First Ukay Together!", use_column_width=True)
-        header_image = Image.open("photos/IMG_3880.JPG")  
+        header_image = Image.open("photos/IMG_3880.jpg")  
         st.image(header_image, caption="First Mass Together!", use_column_width=True)
-        header_image = Image.open("photos/IMG_3902.JPG")  
+        header_image = Image.open("photos/IMG_3902.jpg")  
         st.image(header_image, caption="First Jogging!", use_column_width=True)
     
     # Love Notes Page - Hidden Love Messages
